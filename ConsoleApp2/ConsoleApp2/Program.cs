be06ac18f95e4b2c7203ec28f5da904fafb554a8
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2023-10-18 20:10:40 +0300

    Create Program.cs

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0;i<256;i++)
+            {
+                Console.WriteLine($"Код {i}{(char)i}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
