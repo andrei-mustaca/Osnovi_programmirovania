9db6a8cef04ec95ad97ecc2f4ade38151665e1ba
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2023-10-18 20:17:53 +0300

    Create Program.cs

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1
+            //Console.Write("Введите название футбольного клуба:");
+            //string clubname = Console.ReadLine();
+            //int count = clubname.Length;
+            //Console.WriteLine($"Количество символов в названии клуба:{count}");
+
+            //Задача 2
+            //Console.Write("Введите название города:");
+            //string town = Console.ReadLine();
+            //int count = town.Length;
+            //if(town.Length%2==0)
+            //{
+            //    Console.WriteLine("Количество символов в городе четно");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Количество символов в городе нечетно");
+            //}
+
+            //Задача 3
+            //Console.Write("Введите первую фамилию:");
+            //string surname1 = Console.ReadLine();
+            //Console.WriteLine("Введите вторую фамилию:");
+            //string surname2 = Console.ReadLine();
+            //int count1 = surname1.Length;
+            //int count2 = surname2.Length;
+            //if(count1>count2)
+            //{
+            //    Console.WriteLine("Первая фамилия длиннее второй");
+            //}
+            //else if(count1<count2)
+            //{
+            //    Console.WriteLine("Вторая фамилия длиннее первой");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Фамилии одинаковы по длине");
+            //}
+
+            //Задача 4
+            //string longestword = "a";
+            //string[] words = new string[5];
+            //for (int i = 0; i < words.Length; i++)
+            //{
+            //    Console.WriteLine("Ввести слова:");
+            //    words[i] = Console.ReadLine();
+            //    if(words[0].Length<words[i].Length)
+            //    {
+            //        longestword = words[i];
+            //    }
+            //}
+            //Console.WriteLine("Самое длинное слово:"+longestword);
+
+            //Задача 5
+            //Console.Write("Введите имя:");
+            //string name = Console.ReadLine();
+            //Console.Write("Введите фамилию:");
+            //string surname = Console.ReadLine();
+            //string full_name=name+" "+surname;
+            //Console.WriteLine(full_name);
+
+            //Задача 6
+            //Console.Write("Строка:");
+            //string stroka = Console.ReadLine();
+            //int count = 0;
+            //foreach(char c in stroka)
+            //{
+            //    count++;
+            //}
+            //Console.WriteLine("Количество букв (а,А)-"+count);
+
+            //Задача 7
+
+            Console.ReadKey();
+        }
+    }
+}
