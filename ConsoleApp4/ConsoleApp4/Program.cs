ac32ab521122914fb6c0df2e4249b49d0a159e17
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2023-10-22 19:57:56 +0300

    Create Program.cs

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1
+            //string st2 = "";
+            //for(int i=0;i<5;i++)
+            //{
+            //    Console.WriteLine("Ввести слово:");
+            //    string st1 = Console.ReadLine();
+            //    if(st2.Length<st1.Length)
+            //    {
+            //        st2 = st1 + st2;
+            //    }
+            //}
+            //Console.WriteLine($"Самое длинное слово-{st2}");
+
+            //Задача 2
+            //string stroka = Console.ReadLine();
+            //for(int i=0;i<stroka.Length;i++)
+            //{
+            //    if (stroka[i]=='е'||stroka[i]=='Е')
+            //    {
+            //            stroka = stroka.Replace("Е", "");
+            //            stroka=stroka.Replace("е", "");
+
+            //    }
+            //}
+            //Console.WriteLine($"{stroka}");
+
+            //Задача 3
+            //Console.WriteLine("Введите предложение");
+            //string stroka = Console.ReadLine();
+            //int count = 0;
+            //for(int i=0;i<stroka.Length;i++)
+            //{
+            //    if(stroka[i]=='А'||stroka[i]=='а')
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine($"Количество букв 'А','а'={count}");
+
+            //Задача 4
+            //Console.WriteLine("Введите предложение");
+            //string stroka = Console.ReadLine();
+            //int count = 0;
+            //while(stroka!=" ")
+            //{
+            //    count++;
+            //}
+            //Console.WriteLine("количество букв в первом слове"+count);
+
+            //Задача 6
+            //string stroka = Console.ReadLine();
+            //for (int i = 0; i < stroka.Length; i++)
+            //{
+            //    if (stroka[i] == 'а' || stroka[i] == 'А')
+            //    {
+            //        stroka = stroka.Replace("А", "О");
+            //        stroka = stroka.Replace("а", "о");
+
+            //    }
+            //}
+            //Console.WriteLine($"{stroka}");
+
+            //Задача 7
+            //Console.WriteLine("Введите строку:");
+            //string input = Console.ReadLine();
+            //string reversed = "";
+            //for (int i = input.Length - 1; i >= 0; i--)
+            //{
+            //    reversed += input[i];
+            //}
+            //Console.WriteLine("Реверсированная строка:"+reversed);
+            Console.ReadKey();
+        }
+    }
+}
