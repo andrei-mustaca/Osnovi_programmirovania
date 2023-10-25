c9324beef5d0ee7f644f5c240afb2aabcd53b6c1
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2023-10-26 00:36:17 +0300

    Create Program.cs

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1
+            //Console.WriteLine("Ввести предложение:");
+            //StringBuilder stroka = new StringBuilder(Console.ReadLine());
+            //StringBuilder reverse = new StringBuilder();
+            //for(int i=stroka.Length-1;i>=0;i--)
+            //{
+            //    reverse.Append(stroka[i]);
+            //}
+            //Console.WriteLine($"Реверсированная строка:{reverse}");
+
+            //Задача 2
+            //Console.WriteLine("Ввести предложение:");
+            //StringBuilder stroka = new StringBuilder(Console.ReadLine());
+            //StringBuilder words = new StringBuilder();
+            //for (int i = 0; i <= stroka.Length; i++)
+            //{
+            //    if (stroka[i]!= stroka[stroka.Length-1])
+            //    {
+            //        words.Append(stroka);
+            //    }
+            //}
+            //Console.WriteLine(words);
+            Console.ReadKey();
+        }
+    }
+}
