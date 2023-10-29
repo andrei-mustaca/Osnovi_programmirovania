3efeab748b6c20f898bc70130a3dae8443f29dd5
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2023-10-29 21:28:30 +0200

    Create Program.cs

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1
+            //Console.WriteLine("Введите x=");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите y=");
+            //int y = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"Число {Compar(x,y)} большее");
+
+            //Задача 2
+            //Console.WriteLine("Введите x=");
+            //double x = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите y=");
+            //double y = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите z=");
+            //double z = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine($"Среднее арифметическое={Average(x,y,z)}");
+
+            //Задача 3
+            //double start = -9;
+            //double end = 9; 
+            //double step = 3;
+            //Func(start,end,step);
+            Console.ReadKey();
+        }
+
+        //Задача 1
+        //public static int Compar(int x,int y)
+        //{
+        //    if(x>y)
+        //    {
+        //        return x;
+        //    }
+        //    else
+        //    {
+        //        return y;
+        //    }
+        //}
+
+        //Задача 2
+        //public static double Average(double x,double y,double z)
+        //{
+        //    return (x+y+z) / 3;
+        //}
+
+        //Задача 3
+        //public static void Func(double start,double end,double step)
+        //{
+        //    for (double x = start; x <= end; x += step)
+        //    {
+        //        double y = 4*Math.Pow(x,2) + 5 * x - 10;
+        //        Console.WriteLine($" x={x} \t y={y} ");
+        //    }
+        //}
+    }
+}
