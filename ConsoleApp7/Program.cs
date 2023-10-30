0cb2b9c0d64e57ea8b5c446eb637cbd195603019
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2023-10-30 15:38:13 +0200

    Create Program.cs

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1
+            //symbol();
+
+            //Задача 2
+            //Triangle();
+
+            //Задача 3
+            //double y = Func(5)+Func(12)+Func(19);
+            //Console.WriteLine($"x={y}");
+
+            //Задача 4
+            //Console.Write("Введите a=");
+            //int a=Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите b=");
+            //int b=Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"Число {Compar(a,b)} большее");
+            Console.ReadKey();
+        }
+
+        //Задача 1
+        //public static void symbol()
+        //{
+        //    for(int i=0;i<60;i++)
+        //    {
+        //        Console.Write("*");
+        //    }
+        //}
+
+        //Задача 2
+        //public static void Triangle()
+        //{
+        //    for (int i = 0; i < 8; i++)
+        //    {
+        //        for (int j = 0; j <i; j++)
+        //        {
+        //            Console.Write("#");
+        //        }
+        //        Console.WriteLine();
+        //    }
+        //}
+
+        //Задача 3
+        //public static double Func(double x)
+        //{
+        //    return (Math.Sqrt(x)+x)/2;
+        //}
+
+        //Задача 4
+        //public static int Compar(int a, int b)
+        //{
+        //    if(a>b)
+        //    {
+        //        return a;
+        //    }
+        //    else
+        //    { 
+        //        return b;
+        //    }
+        //}
+    }
+}
