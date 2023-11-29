200b232f25bf68e0134135221054bab07ff79c00
andrei-mustaca <andrejmustaca6@gmail.com>
2023-11-30 02:23:29 +0300

    Create Dog.cs

---
+++ ConsoleApp10/ConsoleApp10/Dog.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Dog
+    {
+        public string Size;
+        public int Age;
+        public string Color;
+        public string Breed;
+        public Dog(string breed, string size, int age, string color)
+        {
+            Breed = breed;
+            Size = size;
+            Age = age;
+            Color = color;
+        }
+
+        // Методы класса
+        public void Eat()
+        {
+            Console.WriteLine($"{Breed} is eating.");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine($"{Breed} is sleeping.");
+        }
+
+        public void Sit()
+        {
+            Console.WriteLine($"{Breed} is sitting.");
+        }
+
+        public void Run()
+        {
+            Console.WriteLine($"{Breed} is running.");
+        }
+    }
+}
