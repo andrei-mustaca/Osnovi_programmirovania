1717db8d39d108d8b95ac3b86780767de2fb510a
andrei-mustaca <andrejmustaca6@gmail.com>
2023-11-30 02:24:09 +0300

    Create Person.cs

---
+++ ConsoleApp10/ConsoleApp10/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Person
+    {
+        public string name;
+        public string surname;
+        public int age;
+        public static void Input(string name,string surname,int age)
+        {
+            Console.WriteLine($"Имя:{name}\t Фамилия:{surname}\t Возраст:{age}");
+        }
+    }
+}
