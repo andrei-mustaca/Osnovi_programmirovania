56be0bb30bb9a9498993fbde4bd7823ff84e2b26
andrei-mustaca <andrejmustaca6@gmail.com>
2023-12-11 15:29:30 +0200

    Create Employee.cs

---
+++ практиум 4/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace практиум_4
+{
+    internal class Employee
+    {
+        public string name;
+        public string surname;
+        public int p;
+        public Employee() { }
+        public Employee( string name, string surname, int p)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.p = p;
+        }
+        public override string ToString()
+        {
+            return $"Имя={name} Фамилия={surname} мин. зарплата={p}";
+        }
+        public int Capital(int k)
+        {
+            return k * p;
+        }
+    }
+}
