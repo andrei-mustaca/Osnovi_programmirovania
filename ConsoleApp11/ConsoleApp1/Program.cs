e8c55b79df097eab83246a34a6709b3198b23fb2
andrei-mustaca <andrejmustaca6@gmail.com>
2023-12-18 03:27:52 +0200

    Create Program.cs

---
+++ ConsoleApp11/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student st1 = new Student("",4);
+            //st1.Fam = "петров";
+            //st1.Kurs = 5;
+            //st1.Name = "";
+            //st1.Age = 120;
+            //st1.ShowInfo();
+
+            //Avto av1 = new Avto();
+            //av1.brand = "Мерседес";
+            //av1.Skor = 0;
+            //av1.color = "зеленый";
+            //av1.ShowInfo();
+
+            //Avto av2 = new Avto();
+            ////av2.brand = "БМВ";
+            ////av2.Skor = 120;
+            ////av2.color = "белый";
+            //av2.ShowInfo();
+
+            //Computer comp1 = new Computer("asd",-1,200);
+            //comp1.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
