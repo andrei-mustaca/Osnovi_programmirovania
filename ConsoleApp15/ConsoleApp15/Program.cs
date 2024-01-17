88a16d02b440469de5149663348a03b33b4d95f0
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-01-17 03:40:39 +0200

    Create Program.cs

---
+++ ConsoleApp15/ConsoleApp15/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Point p1 = new Point();
+            //p1.Print();
+            //Point p2 = new Point(2.3,-5.6);
+            //p2.Print();
+            //Console.WriteLine($"Кол-во созданных экземпляров={Point.count}");
+
+            //Point[] mass = new Point[5];
+            //for (int i = 0; i < mass.Length; i++)
+            //{
+            //    Console.Write("x=");
+            //    double x = Convert.ToDouble(Console.ReadLine());
+            //    Console.Write("y=");
+            //    double y = Convert.ToDouble(Console.ReadLine());
+            //    mass[i] = new Point(x, y);
+            //    mass[i].Print();
+            //}
+            //Console.WriteLine($"Длина={Point.LengthPoints(mass[1].GetX(), mass[1].GetY(), mass[0].GetX(), mass[0].GetY()):f2}");
+            //Console.WriteLine($"Кол-во созданных экземпляров={Point.count}");
+
+            //List<Point> list = new List<Point>(5);
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.Write("x=");
+            //    double x = Convert.ToDouble(Console.ReadLine());
+            //    Console.Write("y=");
+            //    double y = Convert.ToDouble(Console.ReadLine());
+            //    list.Add(new Point(x, y));
+            //    list[i].Print();
+            //}
+            //Console.WriteLine(list.Count);
+            //Console.WriteLine($"Длина={Point.LengthPoints(list[1].GetX(), list[1].GetY(), list[0].GetX(), list[0].GetY()):f2}");
+            //Console.WriteLine($"Кол-во созданных экземпляров={Point.count}");
+            Console.ReadKey();
+        }
+    }
+}
