364f6f62b27d183c532793a964efeb9c047420f3
andrei-mustaca <andrejmustaca6@gmail.com>
2024-01-18 10:59:52 +0200

    Create Program.cs

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Кол-во точек:");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //Point[] mass = new Point[n];
+            //List < Point > points= new List<Point>();
+            //for(int i=0;i</*mass.Length*/4;i++)
+            //{
+            //    Console.WriteLine("Введите x=");
+            //    int x = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Введите y=");
+            //    int y = Convert.ToInt32(Console.ReadLine());
+            //    //mass[i] = new Point(x,y);
+            //    points.Add(new Point(x,y));
+            //}
+            //foreach(var elem in points)
+            //{
+            //    elem.ShowInfo();
+            //}
+            //Point.Count();
+            Console.ReadKey();
+        }
+    }
+}
