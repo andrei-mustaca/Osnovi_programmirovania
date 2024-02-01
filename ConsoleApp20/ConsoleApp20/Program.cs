65499231870134a5c4820530e1b6d0a6c2d60606
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-02-01 04:04:21 +0200

    Create Program.cs

---
+++ ConsoleApp20/ConsoleApp20/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1
+            //Console.Write("Ввести год:");
+            //int year = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Ввести месяц:");
+            //int month = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Ввести день:");
+            //int day = Convert.ToInt32(Console.ReadLine());
+            //DateClass date1 = new DateClass(year,month,day);
+
+            //Console.WriteLine($"Вчерашний день:{date1.Yesterday().ToShortDateString()}");
+            //Console.WriteLine($"Вчерашний день:{date1.Tomorrow().ToShortDateString()}");
+            //date1.Day();
+
+            //Задача 2
+            //Console.Write("Ввести год:");
+            //int year = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Ввести месяц:");
+            //int month = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Ввести день:");
+            //int day = Convert.ToInt32(Console.ReadLine());
+            //DateClass date1 = new DateClass(year, month, day);
+
+            //Console.Write("Ввести год:");
+            //int year2 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Ввести месяц:");
+            //int month2 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Ввести день:");
+            //int day2 = Convert.ToInt32(Console.ReadLine());
+            //DateClass date2 = new DateClass(year2, month2, day2);
+            //date1.Interval(date2);
+
+            //Задача 3
+            //Console.Write("Ввести год:");
+            //int year = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Ввести месяц:");
+            //int month = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Ввести день:");
+            //int day = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Ввести час:");
+            //int hours = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Ввести минуты:");
+            //int minute = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Ввести секунды:");
+            //int seconds = Convert.ToInt32(Console.ReadLine());
+            //MedProc date1 = new MedProc(year,month,day,hours,minute,seconds);
+
+            //Console.Write("Ввести год:");
+            //int year2 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Ввести месяц:");
+            //int month2 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Ввести день:");
+            //int day2 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Ввести час:");
+            //int hours2 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Ввести минуты:");
+            //int minute2 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Ввести секунды:");
+            //int seconds2 = Convert.ToInt32(Console.ReadLine());
+            //MedProc date2 = new MedProc(year2, month2, day2, hours2, minute2, seconds2);
+
+            //TimeSpan interval = date2.Date - date1.Date;
+            //Console.Write("Количество процедур:");
+            //int n = Convert.ToInt32(Console.ReadLine()); 
+            //for(int i=0;i<n;i++)
+            //{
+            //    Console.WriteLine($"Процедура {i+1}:{date1.Date+TimeSpan.FromTicks(interval.Ticks*i)}");
+            //}
+            Console.ReadKey();
+        }
+    }
+}
