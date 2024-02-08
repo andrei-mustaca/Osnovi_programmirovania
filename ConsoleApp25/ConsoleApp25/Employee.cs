4a05ee3fa749bf4957946307c87405b4c1ff960d
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-02-08 23:53:46 +0200

    Create Employee.cs

---
+++ ConsoleApp25/ConsoleApp25/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    class Employee:Person
+    {
+        private string company;
+        public string Company
+        {
+            set { company = value; }
+            get { return company; }
+        }
+        private string job;
+        public string Job
+        {
+            set { job = value; }
+            get { return job; }
+        }
+        public Employee(int age,string name,string surname,string company,string job):base(age,name,surname)
+        {
+            this.company = company;
+            this.job = job;
+        }
+        new public void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Компания:{company} Работа:{job}");
+        }
+    }
+}
