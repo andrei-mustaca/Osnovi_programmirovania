458c41012b6da98a319a810aeb4c00d6fe08b1c6
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-02-08 23:53:11 +0200

    Create Caet.cs

---
+++ ConsoleApp25/ConsoleApp25/Caet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    class Caet
+    {
+        private string name;
+        public string Name
+        {
+            set { name = value; }
+            get { return (name == "") ?"Ошибка":name; }
+        }
+        private int k;
+        public int K
+        {
+            set { k = (value<12&&value>96) ?0:value; }
+            get { return k; }
+        }
+        public Caet() { }
+        public Caet(string name,int k)
+        {
+            this.name = name;
+            this.k = k;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Название:{name} Кол-во:{k}");
+        }
+        public int Cost()
+        {
+            return 15 * k;
+        }
+    }
+}
