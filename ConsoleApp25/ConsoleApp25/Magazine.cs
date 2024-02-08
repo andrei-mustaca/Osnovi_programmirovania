1aed48311391fe43844e1d16ee23f4359136702f
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-02-08 23:53:52 +0200

    Create Magazine.cs

---
+++ ConsoleApp25/ConsoleApp25/Magazine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    class Magazine:Press
+    {
+        private string quality;
+        public string Quality
+        {
+            set { quality = value; }
+            get { return quality; }
+        }
+        public Magazine(int copies,string name,int price,string quality):base(copies,name,price)
+        {
+            this.quality = quality;
+        }
+        new public int Cost()
+        {
+            if(quality=="high")
+            {
+                return base.Cost() + ((base.Cost()/100)*10);
+            }
+            else if(quality=="low")
+            {
+                return base.Cost() - ((base.Cost() / 100) * 10);
+            }
+            else
+            {
+                return base.Cost();
+            }
+        }
+        new public void Output()
+        {
+            base.Output();
+            Console.Write($" Качество:{quality}");
+        }
+    }
+}
