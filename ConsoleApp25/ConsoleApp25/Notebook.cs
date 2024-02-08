00b7cc97a265fbfbf6415666581ed8bed0a06356
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-02-08 23:53:55 +0200

    Create Notebook.cs

---
+++ ConsoleApp25/ConsoleApp25/Notebook.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    class Notebook:Caet
+    {
+        private string material;
+        public string Material
+        {
+            set { material = value; }
+            get { return material; }
+        }
+        public Notebook(string name,int k,string material):base(name,k)
+        {
+            this.material = material;
+        }
+        public void Print()
+        {
+            base.ShowInfo();
+            Console.WriteLine($"Материал:{material}");
+        }
+        new public int Cost()
+        {
+            return base.Cost() + 50;
+        }
+    }
+}
