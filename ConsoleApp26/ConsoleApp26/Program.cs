cfc1e834c7d76af0220eee3893e192ef1c2536a6
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-02-12 05:56:53 +0200

    Create Program.cs

---
+++ ConsoleApp26/ConsoleApp26/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1
+            //Console.Write("Введите кол-во элементов:");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //List <Employee> list= new List<Employee>(a);
+            //bool flag = true;
+            //while(flag)
+            //{
+            //    Console.Write("Статус:");
+            //    string b = Console.ReadLine();
+            //    if(b=="e")
+            //    {
+            //        list.Add(Employee.Enter());
+            //    }
+            //    else if(b=="i")
+            //    {
+            //        list.Add(Injineer.Enter());
+            //    }
+            //    else
+            //    {
+            //        flag = false;
+            //    }
+            //}
+            //foreach(var elem in list)
+            //{
+            //    elem.Print();
+            //    Console.WriteLine($"Доход:{elem.Cost()}");
+            //}
+
+            //Задача 2
+            Console.Write("Введите кол-во элементов:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            List<Avtobus> list = new List<Avtobus>(a);
+            bool flag = true;
+            while (flag)
+            {
+                Console.Write("Статус:");
+                string b = Console.ReadLine();
+                if (b == "a")
+                {
+                    list.Add(Avtobus.Enter());
+                }
+                else if (b == "t")
+                {
+                    list.Add(TuristA.Enter());
+                }
+                else
+                {
+                    flag = false;
+                }
+            }
+            foreach (var elem in list)
+            {
+                elem.Print();
+                Console.WriteLine($"Доход:{elem.Cost()}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
