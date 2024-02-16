6a86d04b2d07aee17049b76952dcd278319b6737
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-02-16 14:30:06 +0200

    Add files via upload

---
+++ ConsoleApp29/AClass-Method().cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    abstract class AClass_Method__
+    {
+        /// <summary>
+        /// Абстрактный метод класса AClass
+        /// </summary>
+        public abstract void Method1();
+    }
+}
---
+++ ConsoleApp29/DerivedClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    /// <summary>
+    /// Наследуемый от класса CClass
+    /// </summary>
+    internal class DerivedClass:CClass
+    {
+        /// <summary>
+        /// метод из класса AClass
+        /// </summary>
+        public override void Method1()
+        {
+            Console.WriteLine("Метод Method1");
+        }
+        /// <summary>
+        /// Метод из класса BClass
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public override int Method2(int a, int b)
+        {
+            return a + b;
+        }
+        /// <summary>
+        /// Метод из класса CClass
+        /// </summary>
+        /// <param name="hello"></param>
+        /// <returns></returns>
+        public override string Method3(string hello)
+        {
+            return hello;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Обычный метод");
+        }
+    }
+}
