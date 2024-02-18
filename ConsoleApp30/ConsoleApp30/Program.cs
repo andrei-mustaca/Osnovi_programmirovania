2c79b110b135a30805740e5ef55147f34a3c6adf
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-02-18 21:49:29 +0200

    Add files via upload

---
+++ ConsoleApp30/ConsoleApp30/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Russian("Иван");
+            Person p2 = new Moldovan("Ионел");
+            Person p3 = new Englishmen("James");
+            p1.Hello();
+            p2.Hello();
+            p3.Hello();
+            Console.ReadKey();
+        }
+    }
+}
