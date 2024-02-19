f0a7fbd456a758b452c8a9c8f6d285adec644523
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-02-19 09:49:33 +0200

    Add files via upload

---
+++ ConsoleApp32/Magazine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp32
+{
+    internal class Magazine:Production
+    {
+        public int tirag;
+        public int Tirag 
+        {
+            get { return tirag; } 
+            set { tirag = value; } 
+        }
+        public int coast;
+        public int Coast
+        {
+            set { coast = value; }
+            get { return coast; }
+        }
+        public Magazine() { }
+        public Magazine(string name,int tirag, int coast):base(name)
+        {
+            this.tirag = tirag;
+            this.coast = coast;
+        }
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"Название:{Name},Тираж:{tirag},Цена за 1 шт:{coast}");
+        }
+        public override double Cost()
+        {
+            return tirag * coast;
+        }
+
+    }
+}
---
+++ ConsoleApp32/Newspaper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp32
+{
+    internal class Newspaper:Production
+    {
+        public int coast1;
+        public int Coast1
+        {
+            set { coast1 = value; }
+            get { return coast1; }
+        }
+        public int kol_vo;
+        public int Kol_vo
+        {
+            set { kol_vo = value; }
+            get { return kol_vo; }
+        }
+        public int tiraj;
+        public int Tiraj
+        {
+            set { tiraj = value; }
+            get { return tiraj; }
+        }
+        public Newspaper() { }
+        public Newspaper(string name,int coast1, int kol_vo,int tiraj):base(name)
+        {
+            this.coast1 = coast1;
+            this.kol_vo = kol_vo;
+            this.tiraj = tiraj;
+        }
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"Название:{Name},Тираж:{tiraj},Кол-во листов:{kol_vo},Цена за 1 лист:{coast1}");
+        }
+        public override double Cost()
+        {
+            return (coast1 * kol_vo) * tiraj;
+        }
+
+    }
+}
