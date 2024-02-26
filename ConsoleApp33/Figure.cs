1180e833785c2a71bc882b77f0dce47aefd531e5
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-02-26 09:49:11 +0200

    Add files via upload

---
+++ ConsoleApp33/Figure.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp33
+{
+    abstract class Figure
+    {
+        private string name;
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+        public Figure(string name)
+        {
+            this.name = name;
+        }
+       
+        public abstract double Area();
+        public abstract double Perimeter();
+        public virtual void Print(){}
+
+    }
+}
