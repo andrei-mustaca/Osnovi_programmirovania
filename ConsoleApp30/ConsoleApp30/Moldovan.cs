fa1c77bfc227abd360d7c51b5a18dd9bb5bb43e8
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-02-29 23:16:38 +0200

    Create Moldovan.cs

---
+++ ConsoleApp30/ConsoleApp30/Moldovan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    class Moldovan:Person
+    {
+        public Moldovan() { }
+        public Moldovan(string name) : base(name)
+        { }
+        public override void Hello()
+        {
+            Console.WriteLine($"Буна зиуа,{Name}");
+        }
+    }
+}
