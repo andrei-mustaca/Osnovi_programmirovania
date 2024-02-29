d1c06db1eabd5a141e1a0e55326406398391f828
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-02-29 23:17:47 +0200

    Create Russian.cs

---
+++ ConsoleApp30/ConsoleApp30/Russian.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    class Russian:Person
+    {
+        public Russian() { }
+        public Russian(string name):base(name)
+        {}
+        public override void Hello()
+        {
+            Console.WriteLine($"Привет,{Name}");
+        }
+    }
+}
