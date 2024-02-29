26849a4c67622afba95f564a8a8aa47fb1babd94
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-02-29 23:26:43 +0200

    Create BClass.cs

---
+++ ConsoleApp29/BClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    /// <summary>
+    /// Абстрактный класс BClass  наследуемый от класса AClass
+    /// </summary>
+    abstract class BClass:AClass_Method__
+    {
+        /// <summary>
+        /// Абстрактный класс для сложения двух чисел
+        /// </summary>
+        /// <param name="a">первое число</param>
+        /// <param name="b">второе число</param>
+        /// <returns>сумма двух значений</returns>
+        public abstract int Method2(int a,int b);
+    }
+}
