20961c29a5c1b96abcf0594c2ea290c330a48231
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-02-29 23:27:00 +0200

    Create Class.cs

---
+++ ConsoleApp29/Class.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    /// <summary>
+    /// Абстрактный класс Class наследуемый от класса BClass
+    /// </summary>
+    abstract class CClass:BClass
+    {
+        /// <summary>
+        /// Абстрактный метод для приветствия 
+        /// </summary>
+        public abstract string Method3(string hello);
+    }
+}
