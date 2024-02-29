7b74327301b52b6b08c8222be7a0eb5390c87815
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-02-29 23:20:19 +0200

    Create Triangle.cs

---
+++ ConsoleApp34/ConsoleApp34/Triangle.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp34
+{
+    class Triangle: Figure
+    {
+        private int side1;
+        private int side2;
+        private int side3;
+        public int Side1
+        {
+            set { side1 = value; }
+            get { return side1; }
+        }
+        public int Side2
+        {
+            set { side2 = value; }
+            get { return side2; }
+        }
+        public int Side3
+        {
+            set { side3 = value; }
+            get { return side3; }
+        }
+        public Triangle(string name, int side1, int side2, int side3) : base(name)
+        {
+            if (side1 < side2 + side3 && side2 < side1 + side3 && side3 < side1 + side2)
+            {
+                this.side1 = side1;
+                this.side2 = side2;
+                this.side3 = side3;
+            }
+            else
+            {
+                this.side1 = 0;
+                this.side2 = 0;
+                this.side3 = 0;
+            }
+        }
+        public override double Area()
+        {
+            int p = (side1 + side2 + side3) / 2;
+            return Math.Sqrt(p * (p - side1) * (p - side2) * (p - side3));
+        }
+        public override double Perimeter()
+        {
+            return side1 + side2 + side3;
+        }
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Название:{Name},Длина первой стороны:{side1},Длина второй стороны:{side2},Длина третьей стороны:{side3},Площадь:{Area()},Периметр:{Perimeter()}");
+        }
+        public static Triangle Enter()
+        {
+            Console.Write("Введите первую сторону:");
+            int side1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите вторую сторону:");
+            int side2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите третью сторону:");
+            int side3 = Convert.ToInt32(Console.ReadLine());
+            return new Triangle("треугольник",side1,side2,side3);
+        }
+    }
+}
