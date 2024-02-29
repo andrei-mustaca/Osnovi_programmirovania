0076aaf3e533516d38ebf28d983e0d34cacffc72
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-02-29 23:30:46 +0200

    Create TXTHandler.cs

---
+++ ConsoleApp31/TXTHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    internal class TXTHandler:AbstractHandler
+    {
+        public override void Open()
+        {
+            Console.WriteLine("Open document format-TXT");
+        }
+        public override void Create()
+        {
+            Console.WriteLine("Creat document format -TXT");
+        }
+        public override void Change()
+        {
+            Console.WriteLine("Change document format -TXT");
+        }
+        public override void Save()
+        {
+            Console.WriteLine("Save document format - TXT");
+        }
+    }
+}
