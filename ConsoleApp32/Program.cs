8be607979449e73192e9c141789f619b2d1c0eba
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-02-29 23:31:29 +0200

    Create Program.cs

---
+++ ConsoleApp32/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Production>productions = new List<Production>();
+            while (true)
+            {
+                Console.Write("Ввести метку:");
+                string metka=Console.ReadLine();
+                switch(metka)
+                {
+                    case "mag":
+                    case "Mag":
+                        
+                        Production production = new Magazine();
+                        break;
+                }
+            }
+        }
+    }
+}
