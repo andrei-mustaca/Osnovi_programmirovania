ee4135a26807347982760e3222729f8d4efd6a9e
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-02-29 23:31:25 +0200

    Create Production.cs

---
+++ ConsoleApp32/Production.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp32
+{
+    abstract class Production
+    {
+        public string Name {  get; set; }
+        public Production() { }
+        public Production(string name)
+        {
+            Name = name;
+        }
+        public abstract void ShowInfo();
+        public abstract double Cost();
+    }
+}
