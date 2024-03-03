d0f9d6440c9c004d0954dd6cbafc759755733836
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-03-03 21:57:13 +0200

    Create Person.cs

---
+++ ConsoleApp36/ConsoleApp36/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp36
+{
+    abstract class Person
+    {
+        private string name;
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+        private string surname;
+        public string Surname
+        {
+            set { surname = value; }
+            get { return surname; }
+        }
+        private DateTime date;
+        public DateTime Date
+        {
+            set { date = value; }
+            get { return date; }
+        }
+        private string faculty;
+        public string Faculty
+        {
+            set { faculty = value; }
+            get { return faculty; }
+        }
+        public Person(string name, string surname, int year, int month, int day, string faculty)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.date = new DateTime(year, month, day);
+            this.faculty = faculty;
+        }
+        public abstract void Print();
+        public abstract int Age();
+    }
+}
