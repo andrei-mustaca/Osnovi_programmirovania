f1a7a40c9afd0461c9a4e093d8d77365a64647ca
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-03-04 23:14:10 +0200

    Create Product.cs

---
+++ ConsoleApp37/ConsoleApp37/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37
+{
+    abstract class Product
+    {
+        public string Name { set; get; }
+        public int Cost { set; get; }
+        public Product(string name,int cost)
+        {
+            Name = name;
+            Cost = cost;
+        }
+        public abstract void Print();
+        public abstract bool ExpirationDate();
+    }
+}
