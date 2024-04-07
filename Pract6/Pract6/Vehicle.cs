af3aa566f66c3d173849b5a929d9bb979b23aefe
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-07 21:42:01 +0200

    Create Vehicle.cs

---
+++ Pract6/Pract6/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract6
+{
+    abstract class Vehicle
+    {
+        public int Price { set; get; }
+        public int Maxspeed { set; get; }
+        public int Year { set; get; }
+        public Vehicle(int price,int maxspeed,int year)
+        {
+            Price = price;
+            Maxspeed = maxspeed;
+            Year = year;
+        }
+        public abstract void Print();
+    }
+}
