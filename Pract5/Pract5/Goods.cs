7bf152c0fc6bd535547d57b339e714f8f5736344
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-07 19:59:21 +0200

    Create Goods.cs

---
+++ Pract5/Pract5/Goods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract5
+{
+    abstract class Goods
+    {
+        public string Name { set; get; }
+        public int Price { set; get; }
+        public Goods(string name,int price)
+        {
+            Name = name;
+            Price = price;
+        }
+        public abstract void Print();
+        public abstract void Deadline();
+    }
+}
