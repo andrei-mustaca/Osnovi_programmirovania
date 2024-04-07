b00c1e68eac490d3dee0f28ea0629557527acbfe
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-07 20:00:03 +0200

    Create Product.cs

---
+++ Pract5/Pract5/Product.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract5
+{
+    class Product:Goods
+    {
+        private DateTime date1;
+        public DateTime Date1 { set; get; }
+        private int date2;
+        public int Date2 { set; get; }
+        public Product(string name,int price,DateTime date1,int date2):base(name,price)
+        {
+            Date1 = date1;
+            Date2 = date2;
+        }
+        public override void Print()
+        {
+            Console.WriteLine($"Название:{Name}");
+            Console.WriteLine($"Цена:{Price}");
+            Console.WriteLine($"Дата производства:{Date1.ToShortDateString()}");
+            Console.WriteLine($"Срок годности:{Date2} дней");
+        }
+        public override void Deadline()
+        {
+            if(DateTime.Now>Date1.AddDays(Date2))
+            {
+                Console.WriteLine(Name);
+            }
+        }
+        public static Product Enter()
+        {
+            Console.Write("Введите название:");
+            string name = Console.ReadLine();
+            Console.Write("Введите цену:");
+            int price = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите дату производства:");
+            DateTime date1 = DateTime.Parse(Console.ReadLine());
+            Console.Write("Введите срок годности:");
+            int date2 = Convert.ToInt32(Console.ReadLine());
+            return new Product(name,price,date1,date2);
+        }
+    }
+}
