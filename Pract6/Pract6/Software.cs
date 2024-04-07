942b9ea84393e034424154dbfb08185b5153ea85
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-07 21:41:54 +0200

    Create Software.cs

---
+++ Pract6/Pract6/Software.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract6
+{
+    abstract class Software
+    {
+        public string Manufacturer { set; get; }
+        public string Name { set; get; }
+        public Software(string manufacturer,string name)
+        {
+            Manufacturer = manufacturer;
+            Name = name;
+        }
+        public abstract void Print();
+        public abstract void Usable();
+    }
+}
