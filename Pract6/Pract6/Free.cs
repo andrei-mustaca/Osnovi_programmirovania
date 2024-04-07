bbdc0e4c18106e20f5a832124f1bd74444675ed8
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-07 21:41:03 +0200

    Create Free.cs

---
+++ Pract6/Pract6/Free.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract6
+{
+    class Free:Software
+    {
+        public Free(string manufacturer,string name):base(manufacturer,name) { }
+        public override void Print()
+        {
+
+            Console.WriteLine($"Название:{Name}");
+            Console.WriteLine($"Производитель");
+        }
+        public override void Usable()
+        {
+            Console.WriteLine(Name);
+        }
+        public static Free Enter()
+        {
+            Console.Write("Введите производителя:");
+            string manufacturer = Console.ReadLine();
+            Console.Write("Введите имя:");
+            string name = Console.ReadLine();
+            return new Free(manufacturer,name);
+        }
+    }
+}
