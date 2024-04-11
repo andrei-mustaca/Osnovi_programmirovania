ff535fb83b05799a277e3b363a48b5ce24326b9f
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-11 17:17:56 +0200

    Create Interface1.cs

---
+++ Practicum1Interface/Practicum1Interface/Interface1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicum1Interface
+{
+    public interface Interface1
+    {
+        void Print();
+        double P();
+        double S();
+    }
+}
