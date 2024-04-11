3b50a7262361e76ef455039562fba72b40a87934
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-11 17:18:00 +0200

    Create Krug.cs

---
+++ Practicum1Interface/Practicum1Interface/Krug.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicum1Interface
+{
+    class Krug:Interface1
+    {
+        const double PI = 3.14;
+        public string Name { set; get; }
+        public int R { set; get; }
+        public int D { set; get; }
+        public Krug(string name,int r,int d)
+        {
+            Name = name;
+            R = r;
+            D = d;
+        }
+        void Interface1.Print()
+        {
+            Console.WriteLine($"Название:{Name},Радиус:{R},Диаметр:{D}");
+        }
+        double Interface1.P()
+        {
+            return PI * D;
+        }
+        double Interface1.S()
+        {
+            return PI * Math.Pow(R,2);
+        }
+        public static Krug Enter()
+        {
+            Console.Write("Введите название:");
+            string name = Console.ReadLine();
+            Console.Write("Введите радиус:");
+            int r = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите диаметр:");
+            int d = Convert.ToInt32(Console.ReadLine());
+            return new Krug(name,r,d);
+        }
+    }
+}
