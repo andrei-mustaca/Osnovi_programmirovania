8333b04361308a727d3563c3f03d099e8c45b4a3
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-11 17:18:55 +0200

    Create Program.cs

---
+++ Practicum1Interface/Practicum1Interface/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicum1Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Interface1> list = new List<Interface1>();
+            bool flag = true;
+            while(flag)
+            {
+                Console.WriteLine("[1] Добавить квадрат");
+                Console.WriteLine("[2] Добавить прямоугольник");
+                Console.WriteLine("[3] Добавить круг");
+                Console.WriteLine("[4] Выход");
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case '1':list.Add(Kvadrat.Enter());
+                        break;
+                    case '2':list.Add(Pramougolnic.Enter());
+                        break;
+                    case '3':list.Add(Krug.Enter());
+                        break;
+                    case '4':flag = false;
+                        break;
+                    default:Console.WriteLine("Ошибка,попробуйте еще раз");
+                        break;
+                }
+            }
+            bool flag1 = true;
+            while(flag1)
+            {
+                Console.WriteLine("[1] Вывод информации");
+                Console.WriteLine("[2] Выход");
+                switch(Console.ReadKey(true).KeyChar)
+                {
+                    case '1':
+                        foreach(var elem in list)
+                        {
+                            elem.Print();
+                            Console.WriteLine($"Площадь:{elem.P()},Периметр(длинна):{elem.S()}");
+                        }
+                        break;
+                    case '2':flag1 = false;
+                        break;
+                    default:Console.WriteLine("Ошибка,повторите еще раз");
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
