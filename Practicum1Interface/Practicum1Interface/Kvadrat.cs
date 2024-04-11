89c630be06e7855cac1bdd4040d423009c94d0ad
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-11 17:18:10 +0200

    Create Kvadrat.cs

---
+++ Practicum1Interface/Practicum1Interface/Kvadrat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicum1Interface
+{
+    class Kvadrat:Interface1
+    {
+        public string Name { set; get; }
+        public int A { set; get; }
+        public Kvadrat(string name,int a)
+        {
+            Name = name;
+            A = a;
+        }
+        void Interface1.Print()
+        {
+            Console.WriteLine($"Название:{Name},Сторона:{A}");
+        }
+        double Interface1.P()
+        {
+            return A * 4;
+        }
+
+        double Interface1.S()
+        {
+            return A * A;
+        }
+        public static Kvadrat Enter()
+        {
+            Console.Write("Введите название:");
+            string name = Console.ReadLine();
+            Console.Write("Введите сторону:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            return new Kvadrat(name,a);
+        }
+    }
+}
