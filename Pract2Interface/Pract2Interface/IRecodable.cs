7651a6cda2443d6bc76a40fd487f8680c9b22315
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-14 08:26:51 +0200

    Create IRecodable.cs

---
+++ Pract2Interface/Pract2Interface/IRecodable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract2Interface
+{
+    interface IRecodable
+    {
+        void Record();
+        void Pause();
+        void Stop();
+    }
+}
