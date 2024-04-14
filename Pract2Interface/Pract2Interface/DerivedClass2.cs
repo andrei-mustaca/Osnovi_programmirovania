4fc70f71d9ba78a69baefb8aafbaba500a541dee
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-14 08:25:45 +0200

    Create DerivedClass2.cs

---
+++ Pract2Interface/Pract2Interface/DerivedClass2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract2Interface
+{
+    class DerivedClass2:IInterface2
+    {
+        public double PI()
+        {
+            return Math.PI;
+        }
+        public int Ret()
+        {
+            return 10;
+        }
+        public double Pow()
+        {
+            Console.Write("Введите x:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            return Math.Pow(x,2);
+        }
+        public double Sqrt()
+        {
+            Console.Write("Введите x:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            return Math.Sqrt(x);
+        }
+    }
+}
