8d98619878f1993383a4c7b6079d1f597d608718
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-14 08:27:57 +0200

    Create TVSet.cs

---
+++ Pract2Interface/Pract2Interface/TVSet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract2Interface
+{
+    class TVSet:ISwitchable
+    {
+        public void Inclusion()
+        {
+            Console.WriteLine("Включение телевизора");
+        }
+        public void Shutdown()
+        {
+            Console.WriteLine("Выключение телевизора");
+        }
+    }
+}
