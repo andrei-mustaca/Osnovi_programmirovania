b3b8968139ef671fd40495de62b2d73b95190f4e
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-14 08:26:25 +0200

    Create IInterface1_3.cs

---
+++ Pract2Interface/Pract2Interface/IInterface1_3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract2Interface
+{
+    interface IInterface1_3:IInterface1_2
+    {
+        void Method3();
+    }
+}
