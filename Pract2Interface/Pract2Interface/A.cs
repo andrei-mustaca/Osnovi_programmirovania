88d175daf50f62608379617dc8ef47d1b28494db
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-14 08:25:15 +0200

    Create A.cs

---
+++ Pract2Interface/Pract2Interface/A.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract2Interface
+{
+    class A:IInteface6_1
+    {
+        public int X { set; get; }
+        public int Y { set; get; }
+        public A() { }
+        public A(int x,int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public virtual int Add()
+        {
+            return X + Y;
+        }
+        public virtual int Sub()
+        {
+            return X - Y;
+        }
+        public virtual int Mul()
+        {
+            return X * Y;
+        }
+        public virtual int Div()
+        {
+            return X / Y;
+        }
+    }
+}
