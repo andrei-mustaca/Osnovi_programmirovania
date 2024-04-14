b33285d55e2e13756d2e91e8576eb5c962d28130
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-14 08:25:57 +0200

    Create IInteface6_1.cs

---
+++ Pract2Interface/Pract2Interface/IInteface6_1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract2Interface
+{
+    interface IInteface6_1
+    {
+        int Add();
+        int Sub();
+        int Mul();
+        int Div();
+    }
+}
