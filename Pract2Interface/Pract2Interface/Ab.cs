7cbf42e55933350140ff6b5b2d110d6c8066fc5a
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-14 08:25:22 +0200

    Create Ab.cs

---
+++ Pract2Interface/Pract2Interface/Ab.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract2Interface
+{
+    class Ab:A,IInterface6_2
+    {
+        public double Pow()
+        {
+            Console.Write("Введите x:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            return Math.Pow(x, 2);
+        }
+        public double Sqrt()
+        {
+            Console.Write("Введите x:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            return Math.Sqrt(x);
+        }
+    }
+}
