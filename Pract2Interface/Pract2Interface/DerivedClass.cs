eb9ab48ceb89469e6d8461bd17cdaeac914a741e
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-14 08:25:41 +0200

    Create DerivedClass.cs

---
+++ Pract2Interface/Pract2Interface/DerivedClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract2Interface
+{
+    class DerivedClass:IInterface
+    {
+        public void Method()
+        {
+            Console.WriteLine("Метод,реализованный в DerivedClass");
+        }
+    }
+}
