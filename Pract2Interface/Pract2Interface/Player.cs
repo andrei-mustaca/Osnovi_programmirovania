68edf0e64a6389b2766cd5bb430703ca54796f43
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-14 08:27:41 +0200

    Create Player.cs

---
+++ Pract2Interface/Pract2Interface/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract2Interface
+{
+    class Player:IPlayable,IRecodable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Игра");
+        }
+        public void Pause()
+        {
+            Console.WriteLine("Пауза");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("Стоп");
+        }
+        public void  Record()
+        {
+            Console.WriteLine("Запись");
+        }
+    }
+}
