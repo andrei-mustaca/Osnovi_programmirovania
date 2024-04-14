671258661f5e164048bfb511825170415dba5eb0
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-14 08:25:53 +0200

    Create DerivedClass4.cs

---
+++ Pract2Interface/Pract2Interface/DerivedClass4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract2Interface
+{
+    class DerivedClass4:IInterface4_1,IInterface4_2
+    {
+        public void SayLoud()
+        {
+            Console.WriteLine("Говорит громко - Я люблю программирование!");
+        }
+        public void Thinking()
+        {
+            Console.WriteLine("Думает - А С# больше всего");
+        }
+        public void SayNormal()
+        {
+            Console.WriteLine("Говорит нормально - Меньше люблю компьютерные игры");
+        }
+    }
+}
