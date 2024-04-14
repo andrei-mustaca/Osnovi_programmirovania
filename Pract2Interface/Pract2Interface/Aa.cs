16f2a06bce0caec9e9687eea24f486f9091fbd33
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-14 08:25:19 +0200

    Create Aa.cs

---
+++ Pract2Interface/Pract2Interface/Aa.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract2Interface
+{
+    class Aa:A
+    {
+        public int Z { set; get; }
+        public Aa(int x,int y,int z):base(x,y)
+        {
+            this.Z = z;
+        }
+        public override int Add()
+        {
+            return base.Add()+Z;
+        }
+        public override int Sub()
+        {
+            return base.Sub()-Z;
+        }
+        public override int Mul()
+        {
+            return base.Mul()*Z;
+        }
+        public override int Div()
+        {
+            return base.Div()/Z;
+        }
+    }
+}
