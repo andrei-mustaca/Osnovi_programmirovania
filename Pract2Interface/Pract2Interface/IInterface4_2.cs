f81fae95fed6922adf602a8bc3cf77d60d36e227
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-14 08:26:37 +0200

    Create IInterface4_2.cs

---
+++ Pract2Interface/Pract2Interface/IInterface4_2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract2Interface
+{
+    interface IInterface4_2
+    {
+        void SayLoud();
+        void SayNormal();
+    }
+}
