ec173aecdb62cb5738148b42c4aadc11c1dfcb76
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-14 08:26:29 +0200

    Create IInterface2.cs

---
+++ Pract2Interface/Pract2Interface/IInterface2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract2Interface
+{
+    interface IInterface2
+    {
+        double PI();
+        int Ret();
+        double Pow();
+        double Sqrt();
+    }
+}
