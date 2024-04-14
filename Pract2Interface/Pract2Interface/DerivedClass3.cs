ce9b485223cc02a08728f4c06541182cd5df5a2c
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-14 08:25:49 +0200

    Create DerivedClass3.cs

---
+++ Pract2Interface/Pract2Interface/DerivedClass3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract2Interface
+{
+    class DerivedClass3:IInterface1_3
+    {
+        void IInterface1_1.Method1()
+        {
+            Console.WriteLine("Метод Method1");
+        }
+        void IInterface1_2.Method2()
+        {
+            Console.WriteLine("Метод Method2");
+        }
+        void IInterface1_3.Method3()
+        {
+            Console.WriteLine("Метод Method3");
+        }
+    }
+}
