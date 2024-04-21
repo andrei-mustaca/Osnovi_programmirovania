aa6fe67db533378dea72dc9c36f388ac31aaa007
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-21 22:19:45 +0200

    Create Worker.cs

---
+++ Pract4/Pract4/Worker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract4
+{
+    class Worker:Main
+    {
+        public DateTime Date1 { set; get; }
+        public DateTime Date2 { set; get; }
+        public Worker(string fio,string fac,DateTime date1,DateTime date2):base(fio,fac)
+        {
+            Date1 = date1;
+            Date2 = date2;
+        }
+        public override void Print()
+        {
+            Console.WriteLine($"ФИО:{FIO},Факультет:{Fac},Дата начала работ:{Date1},Дата конца работ:{Date2}");
+        }
+        public override void RegN()
+        {
+            if(Date2<DateTime.Now)
+            {
+                Console.WriteLine($"Пропуск для работника {FIO} аннулирован");
+            }
+        }
+        public override void RegY()
+        {
+            if (Date2 > DateTime.Now)
+            {
+                Console.WriteLine($"Пропуск для работника {FIO} обновлен");
+            }
+        }
+    }
+}
