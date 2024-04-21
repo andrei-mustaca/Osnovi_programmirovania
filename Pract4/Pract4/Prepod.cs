d43f458cdb2817922a6e2ebcd27b974e30faebd6
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-21 22:19:12 +0200

    Create Prepod.cs

---
+++ Pract4/Pract4/Prepod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract4
+{
+    class Prepod:Main
+    {
+        public string Kaf { set; get; }
+        public Prepod(string fio,string fac,string kaf):base(fio,fac)
+        {
+            Kaf = kaf;
+        }
+        public override void Print()
+        {
+            Console.WriteLine($"ФИО:{FIO},Факультет:{Fac},Кафедра:{Kaf}");
+        }
+        public override void RegN()
+        {}
+        public override void RegY()
+        {
+            Console.WriteLine($"Пропуск для преподователя {FIO} обновлен");
+        }
+    }
+}
