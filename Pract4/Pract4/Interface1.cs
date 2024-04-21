bc31b26bc4b7a6324ef46fccd30c3c8967b2c3f9
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-21 22:18:05 +0200

    Create Interface1.cs

---
+++ Pract4/Pract4/Interface1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract4
+{
+    interface Interface1
+    {
+        void RegY();
+        void RegN();
+    }
+}
