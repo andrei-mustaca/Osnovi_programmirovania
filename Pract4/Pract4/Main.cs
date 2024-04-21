ab9009df9809031be856d7e0417cbf6e90eb714c
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-21 22:18:11 +0200

    Create Main.cs

---
+++ Pract4/Pract4/Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract4
+{
+    abstract class Main:Interface1
+    {
+        public string FIO { set; get; }
+        public string Fac { set; get; }
+        public Main(string fio,string fac)
+        {
+            FIO = fio;
+            Fac = fac;
+        }
+        public abstract void Print();
+        public abstract void RegY();
+        public abstract void RegN();
+    }
+}
