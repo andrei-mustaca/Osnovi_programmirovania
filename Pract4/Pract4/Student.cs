dcd624d920feea760cc53fc8a6c6a3760529231d
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-21 22:19:38 +0200

    Create Student.cs

---
+++ Pract4/Pract4/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract4
+{
+    class Student:Main
+    {
+        public int Year { set; get; }
+        public Student(string fio,string fac,int year):base(fio,fac)
+        {
+            Year = year;
+        }
+        public override void Print()
+        {
+            Console.WriteLine($"ФИО:{FIO},Факультет:{Fac},Год поступления:{Year}");
+        }
+        public override void RegN()
+        {
+            if(Year+4<=DateTime.Now.Year)
+            {
+                Console.WriteLine($"Пропуск для студента {FIO} аннулирован");
+            }
+        }
+        public override void RegY()
+        {
+            if(Year+4>DateTime.Now.Year)
+            {
+                Console.WriteLine($"Пропуск для студента {FIO} обновлен");
+            }
+        }
+    }
+}
