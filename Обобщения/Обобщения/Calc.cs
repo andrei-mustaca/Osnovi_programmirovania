6983d7772cc263738022df6dccc52eeea385c73e
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-25 20:43:04 +0200

    Create Calc.cs

---
+++ Обобщения/Обобщения/Calc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Обобщения
+{
+    class Calc<A,B>
+    {
+        public void Calculator(A x,B y)
+        {
+            dynamic vx = x;
+            dynamic vy = y;
+            Console.WriteLine("[1] Сложение");
+            Console.WriteLine("[2] Вычитание");
+            Console.WriteLine("[3] Умножение");
+            Console.WriteLine("[4] Деление");
+            switch (Console.ReadKey(true).KeyChar)
+            {
+                case '1': Console.WriteLine($"{x}+{y}={vx + vy}");
+                    break;
+                case '2': Console.WriteLine($"{x}-{y}={vx - vy}");
+                    break;
+                case '3': Console.WriteLine($"{x}*{y}={vx * vy}");
+                    break;
+                case '4': Console.WriteLine($"{x}/{y}={vx / vy}");
+                    break;
+            }
+            
+        }
+    }
+}
