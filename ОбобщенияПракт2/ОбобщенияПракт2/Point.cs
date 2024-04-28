3e13bce49b21515200e0e709a6512a8d46aaed2f
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-04-28 20:43:17 +0200

    Create Point.cs

---
+++ ОбобщенияПракт2/ОбобщенияПракт2/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ОбобщенияПракт2
+{
+    class Point<T>
+    {
+        public T X { set; get; }
+        public T Y { set; get; }
+        public Point() { }
+        public Point(T x,T y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
