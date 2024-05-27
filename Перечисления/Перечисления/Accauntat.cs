3c996b10d359e68c7a30939168e4f1bfc2576965
andrei-mustaca <122750846+andrei-mustaca@users.noreply.github.com>
2024-05-27 14:41:15 +0200

    Create Accauntat.cs

---
+++ Перечисления/Перечисления/Accauntat.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Перечисления
+{
+    class Accauntat
+    {
+        public enum Post
+        {
+            Manager=200,
+            Employee=150,
+            Director=250,
+            Admin=225
+        }
+        public static bool AskForBonus(Post worker,int hours)
+        {
+            switch(worker)
+            {
+                case Post.Admin:
+                    if (hours>=(int)Post.Admin)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                case Post.Director:
+                    if(hours>=(int)Post.Director)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                case Post.Employee:
+                    if(hours>=(int)Post.Employee)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                case Post.Manager:
+                    if(hours>=(int)Post.Manager)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                default:return false;
+            }
+        }
+    }
+}
